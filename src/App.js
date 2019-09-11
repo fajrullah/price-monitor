d64265b4f690ca8cfb6928bbc8c56c24cd070777
fajrullah sejati <fajrullahsejati_9_7@yahoo.com>
2019-09-11 11:05:16 +0700

    Comment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Comments from './components/Comments';
+import Reply from './components/Reply';
+import { Container, Header, List } from "semantic-ui-react";
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Container style={{ margin: 20 }}>
+    <Header as="h3">This example is powered by Semantic UI React 😊</Header>
+    <List bulleted>
+      <List.Item
+        as="a"
+        content="💌 Official documentation"
+        href="https://react.semantic-ui.com/"
+        target="_blank"
+      />
+      <List.Item
+        as="a"
+        content="💡 StackOverflow"
+        href="https://stackoverflow.com/questions/tagged/semantic-ui-react?sort=frequent"
+        target="_blank"
+      />
+    </List>
+    <Comments/>
+  </Container>
   );
 }
 
