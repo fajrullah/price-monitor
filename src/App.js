95f041912792b9db15e901dfdd23ac80a8da5aba
fajrullah sejati <fajrullahsejati_9_7@yahoo.com>
2019-09-11 11:07:09 +0700

    [FUNCTION] Product Fab

--- src/App.js
+++ src/App.js
@@ -3,27 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Comments from './components/Comments';
 import Reply from './components/Reply';
-import { Container, Header, List } from "semantic-ui-react";
+import { Container, Header, List, Input } from "semantic-ui-react";
 import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
   <Container style={{ margin: 20 }}>
-    <Header as="h3">This example is powered by Semantic UI React 😊</Header>
-    <List bulleted>
-      <List.Item
-        as="a"
-        content="💌 Official documentation"
-        href="https://react.semantic-ui.com/"
-        target="_blank"
-      />
-      <List.Item
-        as="a"
-        content="💡 StackOverflow"
-        href="https://stackoverflow.com/questions/tagged/semantic-ui-react?sort=frequent"
-        target="_blank"
-      />
-    </List>
+    <Header as="h3">Comments Products</Header>
+      <Input />
     <Comments/>
   </Container>
   );
