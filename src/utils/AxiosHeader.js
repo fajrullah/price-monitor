8d695020d7f85f099e3defa886072fb945df418a
fajrullah sejati <fajrullahsejati_9_7@yahoo.com>
2019-09-11 16:05:25 +0700

    [DB]

--- src/utils/AxiosHeader.js
+++ src/utils/AxiosHeader.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default function withStoredHeaders() {
   return axios.create({
-  	baseURL: 'http://localhost:4000/',
+  	baseURL: 'https://price-rest.herokuapp.com/',
   })
 }
 
